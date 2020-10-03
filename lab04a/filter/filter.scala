d2097d32205bfcfed13314baf09f631f007b0b20
tatiana.dvoryaninova <DVORYANINOVA.T.ALE@SBERBANK.RU>
2020-10-03 18:26:48 +0300

    add lab04a

---
+++ lab04a/filter/filter.scala
@@ -0,0 +1,55 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{from_unixtime, unix_timestamp, _}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.{udf, col}
+
+object filter  extends App {
+
+  override def main(args: Array[String]) = {
+    val sparkSession = SparkSession
+      .builder()
+      .appName("lab04_33")
+      .getOrCreate()
+    import sparkSession.implicits._
+
+    val offset = sparkSession.conf.get("spark.filter.offset")
+    val df = sparkSession
+        .read
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "spark-master-1:6667")
+        .option("subscribe", sparkSession.conf.get("spark.filter.topic_name"))
+        .option("startingOffsets",if (offset == "earliest") s"$offset" else s""" { "lab04_input_data": {"0": $offset } """)
+        .load()
+
+    val jsonString = df
+        .select(col("value")
+        .cast("string")).as[String]
+    val parsed = sparkSession
+        .read
+        .json(jsonString)
+    var withColDate = parsed
+        .select('category,'event_type,'item_id,'item_price,'timestamp,'uid)
+        //.withColumn("date", date_format((from_unixtime(($"timestamp"/1000))).cast("date"), "YYYYMMDD"))
+      .withColumn("date", to_date(($"timestamp"/1000).cast("timestamp"),"YYYYMMDD"))
+        .withColumn("p_date", col("date"))
+
+    val output_dir_prefix = sparkSession.conf.get("spark.filter.output_dir_prefix")
+    withColDate.show(5)
+    println(output_dir_prefix)
+    withColDate
+      .filter('event_type === "view")
+      .write
+      .format("json")
+      .mode("overwrite")
+      .partitionBy("p_date")
+      .save(output_dir_prefix + "/view/")
+
+    withColDate
+      .filter('event_type === "buy")
+      .write
+      .format("json")
+      .mode("overwrite")
+      .partitionBy("p_date")
+      .save(output_dir_prefix + "/buy/")
+  }
+}
