a7e0d45625059bdcf6b6909b14b23fff6453e3a2
tatiana.dvoryaninova <DVORYANINOVA.T.ALE@SBERBANK.RU>
2020-10-18 02:17:06 +0300

    add lab06

--- lab06/features/src/main/scala/features.scala
+++ lab06/features/src/main/scala/features.scala
@@ -1,9 +1,7 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{from_unixtime, unix_timestamp, _}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.functions.{udf, col}
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.linalg.Vectors
+import scala.util.Try
 import org.apache.spark.sql.functions
 
 object features  extends App {
@@ -13,11 +11,12 @@ object features  extends App {
     .getOrCreate()
   import sparkSession.implicits._
 
+  // читаем логи, получаем основные колонки для расчетов
   val webSiteLogs = sparkSession.read.json("/labs/laba03/weblogs.json")
     .select('uid, explode('visits).alias("visits"))
     .withColumn("url", col("visits").getField("url"))
     .withColumn("timestamp", col("visits").getField("timestamp"))
-    .withColumn("date", to_timestamp(to_utc_timestamp(($"timestamp"/1000).cast("timestamp"), "Europe/Moscow"),"YYYYMMDD HH:mm:ss"))
+    .withColumn("date", from_utc_timestamp(($"timestamp"/1000).cast("timestamp"),"Etc/GMT+3"))
     .withColumn("hour", hour(col("date")))
     .withColumn("web_hour", concat(lit("web_hour_"),hour(col("date"))))
     .withColumn("web_day", lower(concat(lit("web_day_"),date_format(col("date"), "E"))))
@@ -27,97 +26,96 @@ object features  extends App {
     .drop('timestamp)
     .withColumn("domain", regexp_replace(lower(callUDF("parse_url", 'url, lit("HOST"))),"www.",""))
     .drop('url)
-    .where(col("uid").isNotNull)
     .where(col("domain").isNotNull)
+    .orderBy('uid,'domain)
 
-  val cWork = webSiteLogs
-    .select('uid, 'web_fract)
-    .filter('web_fract === "WORK")
-    .groupBy('uid)
-    .agg(count('uid))
-    .select('uid, functions.col("count(uid)").alias("web_count_work_hours"))
-  val cEvening = webSiteLogs
-    .select('uid, 'web_fract)
-    .filter('web_fract === "EVENING")
-    .groupBy('uid)
-    .agg(count('uid))
-    .select('uid, functions.col("count(uid)").alias("web_count_evening_hours"))
-  val cAllHours = webSiteLogs
-    .select('uid, 'web_fract)
-    .groupBy('uid)
-    .agg(count('uid))
-    .select('uid, functions.col("count(uid)").alias("web_count_all_hours"))
-  //.alias("web_fraction_all_hours")
-  val fWork = cAllHours
-    .join(cWork, cWork("uid") === cAllHours("uid"), "left")
-    .withColumn("web_fraction_work_hours", 'web_count_work_hours / 'web_count_all_hours)
-    .drop(cWork("uid"))
-    .select('uid,'web_fraction_work_hours)
-  val fEvening = cAllHours
-    .join(cEvening, cEvening("uid") === cAllHours("uid"), "left")
-    .withColumn("web_fraction_evening_hours", 'web_count_evening_hours / 'web_count_all_hours)
-    .drop(cEvening("uid"))
-    .select('uid,'web_fraction_evening_hours)
-  val webSiteLogsFract = fWork
-    .join(fEvening, fEvening("uid") === fWork("uid"), "left")
-    .drop(fEvening("uid"))
-
-  val cDay = webSiteLogs
-    .select('uid, 'web_day)
-    .groupBy('uid)
-    .pivot('web_day)
-    .agg(count('uid))
-  val fDay = cDay
-    .join(webSiteLogsFract, webSiteLogsFract("uid") === cDay("uid"), "left")
-    .drop(webSiteLogsFract("uid"))
-  val cHour = webSiteLogs
-    .select('uid, 'web_hour)
-    .groupBy('uid)
-    .pivot('web_hour)
-    .agg(count('uid))
-  val fDayHour = fDay
-    .join(cHour, cHour("uid") === fDay("uid"), "left")
-    .drop(cHour("uid"))
-
-  val userItems = sparkSession
-    .read
-    .format("parquet")
-    .load("/user/tatiana.dvoryaninova/users-items/*")
-  val userItemsWithFields = userItems
-    .join(fDayHour, fDayHour("uid") === userItems("uid"), "left")
-    .drop(fDayHour("uid"))
-
+  //считаем топ 1000 сайтов
   val webCntTop1000 = webSiteLogs
     .groupBy("domain").count()
     .select('domain, functions.col("count").alias("countAll"))
     .orderBy(-'countAll).limit(1000)
-  //webCnt.show(5)
 
-  val userList = webSiteLogs // список уникальных uid
-    .select("uid")
-    .distinct //35 960
+  // список уникальных uid
+  val userList = webSiteLogs.select("uid").distinct //35 960
 
-  val webCntUser = webSiteLogs//статистика по всем сайтам для пользователей
+  //статистика по всем сайтам для пользователей
+  val webCntUser = webSiteLogs
     .groupBy("uid","domain").count()
     .select('uid,'domain, functions.col("count").alias("countUser"))
 
-  val userListWithTop1000 = webCntTop1000.crossJoin(userList) // для каждого uid 1000 сайтов cnt-35960000
-  //userListWithTop1000.show(5)
+  // для каждого uid есть набор из 1000 сайтов cnt-35960000
+  val userListWithTop1000 = webCntTop1000.crossJoin(userList)
 
   val webCntUserForTop1000 = userListWithTop1000
     .join(webCntUser, Seq("uid", "domain"), "left")
     .na.fill(0)
     .orderBy('uid,'domain,-'countAll)
-    .groupBy("uid").agg(collect_list('countUser).alias("domain_features"))
-    .orderBy('uid)
+    .groupBy("uid")
+    .agg(collect_list('countUser).alias("domain_features"))
 
-  val resList = userItemsWithFields
+  // считаем остальные колонки
+  // количество для рабочих часов
+  val workHours = webSiteLogs
+    .filter('web_fract === "WORK")
+    .groupBy('uid)
+    .agg(count('uid))
+    .select('uid, functions.col("count(uid)").alias("web_count_work_hours"))
+
+  // количество для вечерних часов
+  val eveningHours = webSiteLogs
+    .filter('web_fract === "EVENING")
+    .groupBy('uid)
+    .agg(count('uid))
+    .select('uid, functions.col("count(uid)").alias("web_count_evening_hours"))
+
+  // общее количество часов
+  val countAllHours = webSiteLogs
+    .groupBy('uid)
+    .agg(count('uid))
+    .select('uid, functions.col("count(uid)").alias("web_count_all_hours"))
+
+  // доля для рабочих часов
+  val fractionWork = countAllHours
+    .join(workHours, Seq("uid"), "left")
+    .withColumn("web_fraction_work_hours", Try('web_count_work_hours / 'web_count_all_hours).getOrElse(lit(0)))
+
+  // количество для вечерних часов
+  val fractionEvening = countAllHours
+    .join(eveningHours, Seq("uid"), "left")
+    .withColumn("web_fraction_evening_hours", Try('web_count_evening_hours / 'web_count_all_hours).getOrElse(lit(0)))
+
+  // количество по дням недели разворачиваем в колонки
+  val countByDayOfWeek = webSiteLogs
+    .groupBy('uid)
+    .pivot('web_day)
+    .agg(count('uid))
+
+  // количество по часам разворачиваем в колонки
+  val countByHour = webSiteLogs
+    .groupBy('uid)
+    .pivot('web_hour)
+    .agg(count('uid))
+
+  // читаем логи посещений и покупок с прошлой лабы 5
+  val userItems = sparkSession
+    .read
+    .format("parquet")
+    .load("/user/tatiana.dvoryaninova/users-items/*")
+
+  // все объединяем
+  var result = countByDayOfWeek
+    .join(countByHour, Seq("uid"), "inner")
+    .join(fractionWork, Seq("uid"), "inner")
+    .join(fractionEvening, Seq("uid"), "inner")
     .join(webCntUserForTop1000, Seq("uid"), "inner")
+    .join(userItems, Seq("uid"), "inner")
+    .drop("web_count_all_hours")
+    .drop("web_count_work_hours")
+    .drop("web_count_evening_hours")
     .na.fill(0)
 
-  // Результат пишем в файл
-  resList
-    .write
+  // сохраняем результат
+  result.write
     .format("parquet")
     .mode("overwrite")
     .save("/user/tatiana.dvoryaninova/features")
